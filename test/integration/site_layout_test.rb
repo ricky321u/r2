b490ca7820355de4289f452afd93dee77dedeeff
ricky321u <ricky321u@gmail.com>
2016-10-02 15:51:41 +0800

    basic user model

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,12 +1,5 @@
 require 'test_helper'
-
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-
-class SiteLayoutTest < ActionDispatch::IntegrationTest
-
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -14,5 +7,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-  end 
+  end
 end
